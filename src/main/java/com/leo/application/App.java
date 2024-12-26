d79eacfa25d9b5de55f389a0fe429669bac84447
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-26 20:58:21 -0300

    Position class

--- src/main/java/com/leo/application/App.java
+++ src/main/java/com/leo/application/App.java
@@ -1,7 +1,10 @@
 package com.leo.application;
 
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
