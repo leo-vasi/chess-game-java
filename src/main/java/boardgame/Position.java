eca05e1b1f0498d888cd800bad86d8b33d09635f
Leonardo Silva <levasi.leonardo55@gmail.com>
2024-12-26 21:11:43 -0300

    Board and Piece classes

--- src/main/java/boardgame/Position.java
+++ src/main/java/boardgame/Position.java
@@ -5,7 +5,8 @@ public class Position {
     private int row;
     private int column;
 
-    public Position () {}
+    public Position () {
+    }
 
     public Position(int row, int column) {
         this.column = column;
